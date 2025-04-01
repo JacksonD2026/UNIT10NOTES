7c3586076c319e891fa25a3bb445bfc12d7df82f
jackson <jackson.dobson2026@gbwl.org>
2025-04-01 12:44:46 +0000

    ten point one notes

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Recursive Methods");
+      // neverEnd(); // causes a stack overflow error because infinite loop
    }
+
+   //RECURSIVE methods called themselves to repeat over
+   // to IDENTIFY a recursive method, look for its name again in the curly brackets
+   
+   public static void neverEnd() {
+      System.out.println("Recursion is cool");
+      neverEnd();
+
+      // Contains at least one call to itself in the method body
+   }
+
+
+
+
 }
